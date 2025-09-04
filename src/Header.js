306312399bbae4f8aff7fabd7e9fc9a79c774893
Ami0480 <amifuku80@gmail.com>
2025-09-04 15:04:56 +0800

    Created desktop navbar

--- src/Header.js
+++ src/Header.js
@@ -30,30 +30,45 @@ export default function Header() {
               alt=""
               onClick={() => setMenuOpen(true)}
             />
+            <div className="navbar-desktop desktop-only">
+              <button onClick={() => navigate("/")}>Home</button>
+              <button onClick={() => navigate("/about")}>About</button>
+              <button onClick={() => navigate("/menu")}>Menu</button>
+              <button onClick={() => navigate("/product")}>Product</button>
+              <button
+                onClick={() => navigate("/booking")}
+                className="nav-button"
+              >
+                Book Now
+              </button>
+            </div>
           </div>
         </div>
-      </header>
 
-      <section className={`menu-body ${menuOpen ? "open" : ""} `} id="new-page">
-        <div className="cross-container">
-          <img
-            src="/images/cross.svg"
-            className="cross"
-            id="close-page"
-            alt=""
-            onClick={() => setMenuOpen(false)}
-          />
-        </div>
-        <div className="menu-content">
-          <button onClick={() => navigate("/")}>Home</button>
-          <button onClick={() => navigate("/about")}>About</button>
-          <button onClick={() => navigate("/menu")}>Menu</button>
-          <button onClick={() => navigate("/product")}>Product</button>
-          <button onClick={() => navigate("/booking")} className="nav-button">
-            Book Now
-          </button>
-        </div>
-      </section>
+        <section
+          className={`menu-body ${menuOpen ? "open" : ""} `}
+          id="new-page"
+        >
+          <div className="cross-container">
+            <img
+              src="/images/cross.svg"
+              className="cross"
+              id="close-page"
+              alt=""
+              onClick={() => setMenuOpen(false)}
+            />
+          </div>
+          <div className="navbar-desktop mobile-only">
+            <button onClick={() => navigate("/")}>Home</button>
+            <button onClick={() => navigate("/about")}>About</button>
+            <button onClick={() => navigate("/menu")}>Menu</button>
+            <button onClick={() => navigate("/product")}>Product</button>
+            <button onClick={() => navigate("/booking")} className="nav-button">
+              Book Now
+            </button>
+          </div>
+        </section>
+      </header>
     </div>
   );
 }
